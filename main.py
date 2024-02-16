6d72c8c06954000d56ac3c0c6dd54de28b9e6b4b
Bacoazul <44854129+Bacoazul@users.noreply.github.com>
2024-02-16 22:58:55 +0000

    Change all the code to make it simpler for online

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify, session
 from num2words import num2words
 from gtts import gTTS
 import random
-import time
 import os
 
 app = Flask(__name__)
@@ -14,47 +13,28 @@ def number_to_french_words(number):
   return num2words(number, lang='fr')
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def display_random_number():
-  message = ''
-
-  if request.method == 'POST':
-    user_guess = int(request.form['guess'])
-    if user_guess == session.get('random_num'):
-      message = "Congratulations! You guessed the number correctly."
-      translated_num = number_to_french_words(session.get('random_num'))
-      return jsonify(result=message, translated_num=translated_num)
-    else:
-      message = "That's not correct, but don't give up! Try again!"
-      return jsonify(result=message)
-
   session['random_num'] = random.randint(1, 100)
   tts = gTTS(text=number_to_french_words(session.get('random_num')), lang='fr')
   audio_file_path = "static/translated_number.mp3"
   tts.save(audio_file_path)
-  cache_buster = time.time()
-
-  return render_template('random_number.html',
-                         cache_buster=cache_buster,
-                         message=message)
+  return render_template('random_number.html')
 
 
-@app.route('/guess', methods=['POST'])
-def guess_number():
-  user_guess = int(request.form['guess'])
-  if user_guess == session.get('random_num'):
-    session.pop('random_num', None)  # Clear the session
-    return jsonify(result="Congratulations! You guessed the number correctly.")
-  else:
-    return jsonify(result="That's not correct, but don't give up! Try again!")
-
+@app.route('/refresh', methods=['GET'])
+def refresh_number():
+  session['random_num'] = random.randint(1, 100)
+  tts = gTTS(text=number_to_french_words(session.get('random_num')), lang='fr')
+  audio_file_path = "static/translated_number.mp3"
+  tts.save(audio_file_path)
+  return jsonify(result="Refreshed")
 
 
 @app.route('/reveal', methods=['GET'])
 def reveal_number():
   random_num = session.get('random_num')
   translated_num = number_to_french_words(session.get('random_num'))
-  #return jsonify(translated_num=translated_num)
   return jsonify(translated_num=translated_num, number=random_num)
 
 
